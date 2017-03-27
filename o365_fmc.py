8f977b5a8f4de44bdeee7945881ee8438dc348ae
sam byers <samwbyers@gmail.com>
2017-03-27 14:59:13 -0400

    syntax fixed

--- o365_fmc.py
+++ o365_fmc.py
@@ -1,15 +1,55 @@
 # pull o365 list from internet -> https://support.content.office.net/en-us/static/O365IPAddresses.xml
 # build objects in FMC from list
 import requests
-from xml.etree import ElementTree
+import json
+import sys
+import xmltodict
+import argparse
+
+o365_url = 'https://support.content.office.net/en-us/static/O365IPAddresses.xml'
+
+def get_args():
+
+    parser = argparse.ArgumentParser(description='Get arguments for o365_fmc script.')
+    parser.add_argument('server', type=str, help='IP or DNS of the FMC Server')
+    parser.add_argument('username', type=str, help='Username for FMC.')
+    parser.add_argument('password', type=str, help='Username for FMC.')
+
+    args = parser.parse_args()
+    return args
+
+def get_xml_dict(url):
+
+    r = requests.get(url)
+    dict = xmltodict.parse(r.content)
+    return dict
+
+def auth_fmc(fmc_server, username, password):
+
+    fmc_server = 'https://' + fmc_server
+    r = None
+    headers = {'Content-Type': 'application/json'}
+    api_auth_path = "/api/fmc_platform/v1/auth/generatetoken"
+    auth_url = fmc_server + api_auth_path
+    try:
+        # REST call with SSL verification turned off: 
+        r = requests.post(auth_url, headers=headers, auth=requests.auth.HTTPBasicAuth(username,password), verify=False)
+        auth_headers = r.headers
+        auth_token = auth_headers.get('X-auth-access-token', default=None)
+        if auth_token == None:
+            print("auth_token not found. Exiting...")
+            sys.exit()
+    except Exception as err:
+        print ("Error in generating auth token --> "+str(err))
+        sys.exit()
+     
+    headers['X-auth-access-token']=auth_token
 
 def main():
+    args = get_args()
+    xml_blob = get_xml_dict(o365_url)
+    fmc_token = auth_fmc(fmc_server)
 
-    r = requests.get('https://support.content.office.net/en-us/static/O365IPAddresses.xml')
-    tree = ElementTree.fromstring(r.content)
-
-    for element in tree:
-        print(element.attrib['name'])
         
 if __name__ == "__main__":
     
--- prov.sh
+++ prov.sh
@@ -1,5 +1,6 @@
 sudo apt-get install python-pip -y
 sudo pip install --upgrade pip
 sudo pip install requests
+sudo pip install xmltodict
 sudo apt-get update -y
 git clone https://github.com/sambyers/o365_fmc /home/ubuntu/o365_fmc
