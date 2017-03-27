0c88076ad1b7750dd0960f25ba3c4f4db9700160
sam byers <samwbyers@gmail.com>
2017-03-27 13:25:21 -0400

    init commit

---
+++ o365_fmc.py
@@ -0,0 +1,17 @@
+# pull o365 list from internet -> https://support.content.office.net/en-us/static/O365IPAddresses.xml
+# build objects in FMC from list
+import requests
+import xml.etree.ElementTree as et
+
+def main()
+
+	r = requests.get('https://support.content.office.net/en-us/static/O365IPAddresses.xml')
+	xml_blob = r.content
+	tree = et.parse(xml_blob)
+	root = tree.getroot()
+
+	for product in root:
+		print product
+
+if __name__ == "__main__":
+	main()
---
+++ prov.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python-pip -y
+sudo pip install --upgrade pip
+sudo pip install requests
+sudo apt-get update -y
+git clone https://github.com/sambyers/o365_fmc /home/ubuntu/o365_fmc
