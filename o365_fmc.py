320e2c07686b589431da1a0012c97f6bfe78e3ed
sam byers <samwbyers@gmail.com>
2017-03-27 15:26:45 -0400

    testing dict

--- o365_fmc.py
+++ o365_fmc.py
@@ -58,8 +58,8 @@ def main():
 
     xml_dict = get_xml_dict(o365_url)
     for p in xml_dict['products']['product']:
-        for i,o in p:
-            print(i,o)
+        for i in p:
+            print(i[1])
     #headers = auth_fmc(fmc_server, username, password)
 
 if __name__ == "__main__":
