328051753604ab27ee67617dcc76b244fa4f7777
sam byers <samwbyers@gmail.com>
2017-03-29 15:00:33 -0400

    working on fmc class

--- prov.sh
+++ prov.sh
@@ -1,10 +1,11 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo pip3 install --upgrade pip
-sudo pip3 install requests
-sudo pip3 install xmltodict
+sudo apt-get install python
+sudo apt-get install python-pip
+sudo pip install --upgrade pip
+sudo pip install requests
+sudo pip install xmltodict
+sudo pip install rainbow_logging_handler
 git clone https://github.com/sambyers/o365_fmc /home/ubuntu/o365_fmc
 cd /home/ubuntu/o365_fmc
 sudo git pull
